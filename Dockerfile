a159035276b6bf7c2dc338b8223bc73f516331d1
Naillton <Nailton_junior@protonmail.com>
2022-08-06 11:00:06 -0300

    back-end

---
+++ Dockerfile
@@ -0,0 +1,3 @@
+FROM node:14-alpine
+
+CMD ["npm", "start"]
---
+++ index.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+
+const strangerThingsDataset = require('./data/dataset/stranger-things-characters.json');
+const StrangerThingsRepository = require('./data/repository/StrangerThings');
+const StrangerThingsService = require('./services/StrangerThings');
+
+const app = express();
+
+const port = process.env.PORT;
+
+const strangerThingsRepository = new StrangerThingsRepository(
+  strangerThingsDataset,
+);
+const strangerThingsService = new StrangerThingsService(
+  strangerThingsRepository,
+);
+
+app.use(cors());
+
+const hereIsTheUpsideDown = JSON.parse(process.env.UPSIDEDOWN_MODE);
+
+app.get('/', (req, res) => {
+  const characters = strangerThingsService.search(
+    req.query,
+    hereIsTheUpsideDown,
+  );
+
+  res.status(200).json(characters);
+});
+
+app.listen(port, () => {
+  console.log('Escutando na porta 3000');
+});
